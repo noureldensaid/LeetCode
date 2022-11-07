d8e7db4983bd1cc3ec9c890ce1ba290036fe1bb0
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2022-11-07 23:08:59 +0200

    Time: 149 ms (92.97%), Space: 33.9 MB (86.37%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.kt
+++ 0392-is-subsequence/0392-is-subsequence.kt
@@ -2,12 +2,17 @@ class Solution {
   fun isSubsequence(s: String, t: String): Boolean {
     var a = 0
     var b = 0
-    while (b < t.length && a < s.length) {
+    if (s.isEmpty()) return true
+    if (t.isEmpty()) return false
+    while (b < t.length) {
         if (s[a] == t[b]) {
             a++
             b++
-        } else b++
+        } else {
+            b++
+        }
+        if (a == s.length) return true
     }
-    return a == s.length
-  }
+    return false}
+
 }
