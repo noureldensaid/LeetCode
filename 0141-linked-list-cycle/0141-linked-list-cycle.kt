3cefe8fbcf6d8753f3ed41bbf326b6270bb07867
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2022-11-10 08:02:15 +0200

    Time: 328 ms (79.48%), Space: 43.7 MB (34.66%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.kt
+++ 0141-linked-list-cycle/0141-linked-list-cycle.kt
@@ -10,15 +10,13 @@
 
 class Solution {
     fun hasCycle(head: ListNode?): Boolean {
-        var slow = head
-        var fast = head
-        while (slow != null && fast != null) {
-            slow = slow?.next
-            fast = fast?.next?.next
-            if (slow!= null && fast != null && slow == fast) {
-                return true
-            }
-        }
-        return false
+  var fast = head
+    var slow = head
+    while (fast!=null && fast.next!=null) {
+        slow = slow!!.next
+        fast = fast.next!!.next
+        if (slow == fast) return true
+    }
+    return false
     }
 }
