4f64acdfd3e430c5252fbe03f37bed79c001d61c
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2022-11-10 04:17:24 +0200

    Time: 151 ms (93.89%), Space: 33.8 MB (82.76%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.kt
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.kt
@@ -8,14 +8,15 @@
  * }
  */
 class Solution {
-   fun middleNode(head: ListNode?): ListNode? {
-    var slow = head
-    var fast = head
-    while (fast != null && fast.next != null) {
-        slow = slow!!.next
-        fast = fast.next!!.next
+  fun middleNode(head: ListNode?): ListNode? {
+   
+    var fast: ListNode? = head
+    var slow: ListNode? = head
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
     }
     return slow
-}
+    }
 
 }
