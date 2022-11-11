11a552ac37d631113f119cbbb98e792e0372fbee
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2022-11-11 17:56:05 +0200

    Time: 233 ms (98.01%), Space: 37.8 MB (94.36%) - LeetHub

--- 0136-single-number/0136-single-number.kt
+++ 0136-single-number/0136-single-number.kt
@@ -1,8 +1,10 @@
 class Solution {
   fun singleNumber(nums: IntArray): Int {
-    var ans = 0
-    for (i in nums.indices) ans = ans xor nums[i]
-    return ans
+     var result = 0
+        for (i in nums) {
+            result = result xor i    
+        }
+        return result
+    }
 }
 
-}
