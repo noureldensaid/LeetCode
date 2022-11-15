243447aa5b74e65f3676244c0c44932f1a526432
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2022-11-15 14:57:35 +0200

    Time: 183 ms (88.53%), Space: 34.9 MB (78.44%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.kt
+++ 0409-longest-palindrome/0409-longest-palindrome.kt
@@ -1,10 +1,10 @@
 class Solution {
-  fun longestPalindrome(s: String?): Int {
+fun longestPalindrome(s: String?): Int {
     if (s == null || s.isEmpty()) return 0
     val set = HashSet<Char>()
     var count = 0
     for (i in s.indices) {
-        if (set.contains(s[i])) {
+        if (s[i] in set) {
             set.remove(s[i])
             count++
         } else set.add(s[i])
