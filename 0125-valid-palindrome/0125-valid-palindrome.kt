c1bdc4d210f30762cd2c3639458e4aadedc1753c
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2023-01-23 02:11:04 +0200

    Time: 310 ms (39.80%), Space: 39.3 MB (36.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,15 +1,13 @@
 class Solution {
-
-     fun isPalindrome(s: String): Boolean {
+  fun isPalindrome(s: String): Boolean {
         var i = 0
         var j = s.length -1
         while(i < j){
             while (i < j && !s[i].isLetterOrDigit()) i++
             while (i < j && !s[j].isLetterOrDigit()) j--
-            if(s[i++].toLowerCase() != s[j--].toLowerCase()) return false
+            if(s[i++].toLowerCase() != s[j--].toLowerCase()) return                 false
         }
         return true
     }
-    
     
 }
