933e4ba1ec38ec4d39f42a54277999d5f59cc15d
NourEldenSaied <93207605+noureldensaid@users.noreply.github.com>
2023-01-23 02:05:35 +0200

    Time: 191 ms (99.00%), Space: 35.8 MB (92.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -1,7 +1,15 @@
 class Solution {
-   val re = Regex("[^A-Za-z0-9]")
-    fun isPalindrome(s: String): Boolean {
-        val cleanString = re.replace(s, "").toLowerCase()
-        return if (cleanString == cleanString.reversed()) true else false
+
+     fun isPalindrome(s: String): Boolean {
+        var i = 0
+        var j = s.length -1
+        while(i < j){
+            while (i < j && !s[i].isLetterOrDigit()) i++
+            while (i < j && !s[j].isLetterOrDigit()) j--
+            if(s[i++].toLowerCase() != s[j--].toLowerCase()) return false
+        }
+        return true
     }
+    
+    
 }
