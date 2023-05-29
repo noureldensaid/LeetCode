de0881f4baae710110b55f8861a8094e9583e872
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-30 02:19:06 +0300

    Time: 1 ms (99.98%), Space: 45.3 MB (7.60%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,13 @@
 class Solution {
-public void moveZeroes(int[] nums) {
+ public void moveZeroes(int[] nums) {
     int left = 0;
-    int right = 0;
-    while (right <= nums.length - 1) {
+    for (int right = 0; right < nums.length; right++) {
         if (nums[right] != 0) {
             int temp = nums[right];
             nums[right] = nums[left];
             nums[left] = temp;
             left++;
         }
-        right++;
     }
 }
 }
