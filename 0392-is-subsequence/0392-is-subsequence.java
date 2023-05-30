fb833930de04071be563cab6d98c02436bb45e7e
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-30 03:41:37 +0300

    Time: 2 ms (57.36%), Space: 41 MB (11.50%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,13 @@
 class Solution {
      public boolean isSubsequence(String s, String t) {
-        String res = "";
-        int i = 0;
+         int i = 0;
         int y = 0;
         while (i < s.length() && y < t.length()) {
             if (s.charAt(i) == t.charAt(y)) {
-                res += s.charAt(i);
-                i++;
+                 i++;
             }
             y++;
         }
-        return res.equals(s);
+        return i==s.length();
     }
 }
