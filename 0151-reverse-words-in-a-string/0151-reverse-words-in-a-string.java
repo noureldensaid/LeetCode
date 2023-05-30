92360a5f58bbf9cf79f1ea79325b9246614b8799
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-30 22:18:27 +0300

    Time: 75 ms (5.31%), Space: 44.5 MB (5.53%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+     public String reverseWords(String s) {
+    char[] text = s.trim().toCharArray();
+    String result = "";
+    int i = 0;
+    int n = text.length;
+    while (i < n) {
+        while (i < n && text[i] == ' ') {
+            i++;
+        }
+        int j = i;
+        while (j < n && text[j] != ' ') {
+            j++;
+        }
+        if (result.isEmpty()) {
+            String sub = String.valueOf(text).substring(i, j);
+            result = sub;
+        } else {
+            String sub = String.valueOf(text).substring(i, j);
+            result = sub + " " +result;
+        }
+        i = j + 1;
+    }
+    return result;
+}
+
+}
