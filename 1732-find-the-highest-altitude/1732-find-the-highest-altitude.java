6d2d83d5d105fe0f1793fedd70b81fcd96086909
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-30 18:42:40 +0300

    Time: 0 ms (100.00%), Space: 40.8 MB (17.15%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,18 +1,18 @@
 class Solution {
-public int largestAltitude(int[] gain) {
-    int max = Integer.MIN_VALUE;
-    int current = 0;
-    int[] results = new int[gain.length + 1];
-    results[0] = current;
+ public int largestAltitude(int[] gain) {
+    int max = Integer.MIN_VALUE; // Variable to track the maximum altitude
+    int current = 0; // Variable to keep track of the current altitude
+    int[] results = new int[gain.length + 1]; // Array to store the altitudes
+    results[0] = current; // Set the initial altitude to 0
     for (int i = 1; i < results.length; i++) {
         current += gain[i - 1]; // Increment i only once and access gain[i-1]
-        results[i] = current;
+        results[i] = current; // Store the current altitude in the results array
     }
     for (int element : results) {
-        max = Math.max(element, max);
+        max = Math.max(element, max); // Find the maximum altitude
     }
-    return max;
+    return max; // Return the maximum altitude
 }
 
-
 }
+ 
