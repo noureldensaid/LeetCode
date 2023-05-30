d84cd86d8eebda5d9ed5c81e44bb7fcdbf106eac
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-31 00:49:24 +0300

    Time: 4 ms (81.34%), Space: 55.4 MB (17.07%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -5,17 +5,24 @@ class Solution {
     int left = 0;
     int area = 0;
     int right = height.length - 1;
+
+    // Use two pointers, one starting from the left and the other from the right
     while (left < right) {
-        w = right - left;  // Calculate the width correctly
-        h = Math.min(height[left], height[right]);  // Calculate the minimum height
-        area = Math.max(area, h * w);
+        w = right - left;  // Calculate the width as the distance between the two pointers
+        h = Math.min(height[left], height[right]);  // Calculate the height as the minimum of the two heights
+        area = Math.max(area, h * w);  // Update the maximum area if necessary
+
+        // Move the pointer that corresponds to the shorter height
         if (height[left] < height[right]) {
             left++;
         } else {
             right--;
         }
     }
+
+    // Return the maximum area
     return area;
 }
-
+ 
 }
+ 
