328964d7f5758b6afe6d98718b481281a239e37a
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-30 05:47:36 +0300

    Time: 5 ms (65.73%), Space: 54.8 MB (53.09%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,13 +1,14 @@
 class Solution {
-      public double findMaxAverage(int[] nums, int k) {
-        double sum = 0;
-        for (int i = 0; i < k; i++) sum += nums[i];
-        double res = sum;
-        for (int i = k; i < nums.length; i++) {
-            sum += nums[i] - nums[i - k];
-            res = Math.max(sum, res);
+        public double findMaxAverage(int[] nums, int k) {
+        double sum=0;
+        for(int i=0;i<k;i++)
+            sum+=nums[i];
+        double res=sum;
+        for(int i=k;i<nums.length;i++){
+            sum+=nums[i]-nums[i-k];
+                res=Math.max(res,sum);
         }
-        return res / k;
+        return res/k;
     }
 }
 
@@ -15,14 +16,3 @@ class Solution {
 
 
 
-//     public double findMaxAverage(int[] nums, int k) {
-//         double sum=0;
-//         for(int i=0;i<k;i++)
-//             sum+=nums[i];
-//         double res=sum;
-//         for(int i=k;i<nums.length;i++){
-//             sum+=nums[i]-nums[i-k];
-//                 res=Math.max(res,sum);
-//         }
-//         return res/k;
-//     }
