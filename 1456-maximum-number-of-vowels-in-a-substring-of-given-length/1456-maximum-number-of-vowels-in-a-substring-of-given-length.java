74549e66f9c5cb8854e58f5da1e0a0f267bb3097
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-05-31 03:15:01 +0300

    Time: 16 ms (61.85%), Space: 43.9 MB (5.82%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,21 +1,28 @@
 class Solution {
-    public int maxVowels(String s, int k) {
-
-        int[] vowels = new int[128];
-        vowels['a'] = vowels['e'] = vowels['i'] = vowels['o'] =     vowels['u'] = 1;
+    public static int maxVowels(String s, int k) {
+        int max = 0;
         int count = 0;
-        int maxCount=0;
+        for (int i = 0; i < k; ++i) {
+            if (isVowel(s.charAt(i)))
+                count++;
+        }
 
-        for(int i=0; i<k; i++){
-            count += vowels[s.charAt(i)];
+        max = count;
+
+        for (int i = k; i < s.length(); ++i) {
+            if (isVowel(s.charAt(i - k)))
+                count--;
+            if (isVowel(s.charAt(i)))
+                count++;
+            max = Math.max(max, count);
         }
-        maxCount = count;
         
-        for(int i=k; i<s.length(); i++){
-            count += vowels[s.charAt(i)] - vowels[s.charAt(i-k)];
-            maxCount = Math.max(maxCount, count);
-            if(maxCount == k) return maxCount;
-        }
-        return maxCount;
+        return max;
+    }
+
+    public static boolean isVowel(char c) {
+
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O'
+                || c == 'U');
     }
 }
