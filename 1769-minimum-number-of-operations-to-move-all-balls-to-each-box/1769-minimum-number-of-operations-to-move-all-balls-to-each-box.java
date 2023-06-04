088a380196b4ecf1da18a94c7aa33e7005dd9cbb
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-04 22:54:52 +0300

    Time: 3 ms (94.81%), Space: 43.7 MB (18.26%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -2,23 +2,22 @@ class Solution {
 public int[] minOperations(String boxes) {
     int n = boxes.length();
     int[] answer = new int[n];
-
-    // Calculate the number of operations to move balls to the left of each box
-    int count = boxes.charAt(0) - '0';
     int operations = 0;
-    for (int i = 1; i < n; i++) {
-        operations += count;
-        answer[i] = operations;
+    int count = 0;
+ 
+    // Calculate the number of operations to move balls to the left of each box
+    for (int i = 0; i < n; i++) {
+        answer[i] += operations;
         count += boxes.charAt(i) - '0';
+        operations += count;
     }
 
     // Calculate the number of operations to move balls to the right of each box
-    count = boxes.charAt(n - 1) - '0';
-    operations = 0;
-    for (int i = n - 2; i >= 0; i--) {
-        operations += count;
-        answer[i] += operations;
+    operations = 0; count = 0;
+     for (int i = n-1 ; i >= 0; i--) {
+       answer[i] += operations;
         count += boxes.charAt(i) - '0';
+        operations += count;
     }
 
     return answer;
