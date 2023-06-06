6350e03d474df63c5d4498ec28d89d412126ab86
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-06 04:05:39 +0300

    Time: 331 ms (100.00%), Space: 41.4 MB (98.52%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -1,6 +1,5 @@
 class Solution {
-    fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-        val ans = mutableListOf<List<Int>>()
+  fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
         val ans1 = mutableListOf<Int>()
         val ans2 = mutableListOf<Int>()
         val set1 = mutableSetOf<Int>()
@@ -10,20 +9,12 @@ class Solution {
         for (n in nums2) set2.add(n)
 
         for (n in set1) {
-            if (!set2.contains(n)) {
-                ans1.add(n)
-            }
+            if (!set2.contains(n)) ans1.add(n)
         }
 
         for (n in set2) {
-            if (!set1.contains(n)) {
-                ans2.add(n)
-            }
+            if (!set1.contains(n)) ans2.add(n)
         }
-
-        ans.add(ans1)
-        ans.add(ans2)
-        return ans
+        return listOf(ans1, ans2)
     }
-
 }
