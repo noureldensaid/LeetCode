266519790c74fe7154516b6bee9ea80df2d3fc30
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-06 03:55:38 +0300

    Time: 378 ms (97.03%), Space: 41.2 MB (98.52%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.kt
@@ -1,29 +1,29 @@
 class Solution {
- fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
-    val ans: MutableList<MutableList<Int>> = ArrayList()
-    val ans1: MutableList<Int> = ArrayList()
-    val ans2: MutableList<Int> = ArrayList()
-    val set1: MutableSet<Int> = HashSet()
-    val set2: MutableSet<Int> = HashSet()
+    fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+        val ans = mutableListOf<List<Int>>()
+        val ans1 = mutableListOf<Int>()
+        val ans2 = mutableListOf<Int>()
+        val set1 = mutableSetOf<Int>()
+        val set2 = mutableSetOf<Int>()
 
-    for (n in nums1) set1.add(n)
-    for (n in nums2) set2.add(n)
+        for (n in nums1) set1.add(n)
+        for (n in nums2) set2.add(n)
 
-    for (n in set1) {
-        if (!set2.contains(n)) {
-            ans1.add(n)
+        for (n in set1) {
+            if (!set2.contains(n)) {
+                ans1.add(n)
+            }
         }
-    }
 
-    for (n in set2) {
-        if (!set1.contains(n)) {
-            ans2.add(n)
+        for (n in set2) {
+            if (!set1.contains(n)) {
+                ans2.add(n)
+            }
         }
-    }
 
-    ans.add(ans1)
-    ans.add(ans2)
-    return ans
-}
+        ans.add(ans1)
+        ans.add(ans2)
+        return ans
+    }
 
 }
