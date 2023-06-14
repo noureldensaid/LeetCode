fd1c827e7e33e6556dbd4950606d0ef67a2c2608
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-14 16:33:59 +0300

    Time: 281 ms (33.33%), Space: 38.5 MB (66.67%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.kt
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.kt
@@ -1,6 +1,6 @@
 class Solution {
  fun frequencySort(nums: IntArray): IntArray {
-    val frequencyMap = mutableMapOf<Int, Int>().withDefault { 0 }
+    val frequencyMap = hashMapOf<Int, Int>().withDefault { 0 }
 
     for (num in nums) {
         frequencyMap[num] = frequencyMap.getValue(num) + 1
