8983dfbc92be71cfe71ffd08d6ed408bb5248e36
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-14 17:01:26 +0300

    Time: 313 ms (11.11%), Space: 38.8 MB (66.67%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.kt
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.kt
@@ -1,14 +1,16 @@
 class Solution {
  fun frequencySort(nums: IntArray): IntArray {
+    // Create a mutable map to store the frequency of each number
     val frequencyMap = mutableMapOf<Int, Int>().withDefault { 0 }
 
+    // Count the frequency of each number in the input array
     for (num in nums) {
         frequencyMap[num] = frequencyMap.getValue(num) + 1
     }
 
-    // negative sign to sort in descinding order
-    return nums.sortedWith(compareBy({ frequencyMap.getValue(it) }, { -it}))
-         .toIntArray()
+    // Sort the numbers based on frequency (ascending) and value (descending)
+    return nums.sortedWith(compareBy({ frequencyMap.getValue(it) }, { -it }))
+        .toIntArray()
 }
 
 }
