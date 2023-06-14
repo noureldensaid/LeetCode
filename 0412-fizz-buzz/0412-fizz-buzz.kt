8b3bb60b58fc80b54f117a685595eb33ad956d56
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-14 17:57:38 +0300

    Time: 221 ms (8.31%), Space: 37.9 MB (64.78%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.kt
+++ 0412-fizz-buzz/0412-fizz-buzz.kt
@@ -1,13 +1,14 @@
 class Solution {
- fun fizzBuzz(n: Int): List<String> {
+fun fizzBuzz(n: Int): List<String> {
     val result = mutableListOf<String>()
     for (i in 1..n) {
-        when {
-            i % 3 == 0 && i % 5 == 0 -> result.add("FizzBuzz")
-            i % 3 == 0 -> result.add("Fizz")
-            i % 5 == 0 -> result.add("Buzz")
-            else -> result.add("$i")
+        val value = when {
+            i % 3 == 0 && i % 5 == 0 -> "FizzBuzz"
+            i % 3 == 0 -> "Fizz"
+            i % 5 == 0 -> "Buzz"
+            else -> i.toString()
         }
+        result.add(value)
     }
     return result
 }
