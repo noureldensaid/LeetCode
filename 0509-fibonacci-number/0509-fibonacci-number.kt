5f591b46596ddb8b30bef174e54e483d990c327d
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-15 15:59:21 +0300

    Time: 164 ms (5.96%), Space: 33 MB (74.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.kt
+++ 0509-fibonacci-number/0509-fibonacci-number.kt
@@ -1,9 +1,11 @@
 class Solution {
 fun fib(n: Int): Int {
-    if (n <= 1) {
-        return n
+    val fib = hashMapOf<Int, Int>().withDefault { 0 }
+    fib[0] = 0
+    fib[1] = 1
+    for (i in 2..n) {
+        fib[i] = fib[i - 1]!! + fib[i - 2]!!
     }
-    return fib(n - 1) + fib(n - 2)
+    return fib[n]!!
 }
-
 }
