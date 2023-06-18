3df8b7834b5293d9168c9c8b4905316afd837d59
Nour Elden Said <sim.noureldinsaid3155@alexu.edu.eg>
2023-06-18 20:04:24 +0300

    Time: 320 ms (58.33%), Space: 40.1 MB (62.50%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.kt
+++ 2418-sort-the-people/2418-sort-the-people.kt
@@ -1,14 +1,10 @@
 class Solution {
- fun sortPeople(names: Array<String>, heights: IntArray): Array<String> {
+  fun sortPeople(names: Array<String>, heights: IntArray): Array<String> {
     val hashmap = hashMapOf<Int, String>()
-    for (i in names.indices) {
-        hashmap[heights[i]] = names[i]
-    }
+    for (i in names.indices) hashmap.put(heights[i], names[i])
     val sortedHeights = heights.sortedDescending()
     val result = Array<String>(names.size) { "null" }
-    for (i in sortedHeights.indices) {
-        result[i] = hashmap[sortedHeights[i]]!!
-    }
+    for (i in sortedHeights.indices) result[i] = hashmap.get(sortedHeights[i])!!
     return result
 }
 }
